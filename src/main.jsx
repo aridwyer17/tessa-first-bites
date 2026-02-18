490caf4293fba6f2c5d02bd48c8488562961710e
aridwyer17 <ariane.dwyer@gmail.com>
2026-02-18 10:20:43 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TessaFirstBites from './TessaFirstBites.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TessaFirstBites />
+  </React.StrictMode>
+)
